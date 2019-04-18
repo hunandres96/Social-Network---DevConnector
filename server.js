c5ab56482ad302ffc504042e138ddedb8c2936ff
Andres Hun <hunandres96@hotmail.com>
2019-04-18 14:57:35 -0500

    Creatiion of the user schema

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const users = require('./routes/api/users');
-const profiles = require('./routes/api/profiles');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log('Mongo DB Connected'))
+  .then(() => console.log("Mongo DB Connected"))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello MERN world!'));
+app.get("/", (req, res) => res.send("Hello MERN world!!!"));
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profiles', profiles);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profiles", profiles);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
